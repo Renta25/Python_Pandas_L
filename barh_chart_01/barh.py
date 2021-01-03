83af5493a64045d2d762bb021c147f0a58df6baf
Arkadiusz Bryska <arkadiusz.bryska@gmail.com>
2021-01-03 17:14:41 +0100

    horizontal_bar_01

---
+++ barh_chart_01/barh.py
@@ -0,0 +1,21 @@
+
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+
+# %% Data
+
+__data = {
+        "North America" : 1500,
+        "South America" : 500,
+        "Europe" : 1500,
+        "Asia" : 2000,
+        "Australia and Oceania" : 1000,
+        "Africa": 500,
+        "Antartica" : 1
+}
+s_data = pd.Series(__data, name="Sell")
+
+# %% Drowing horizontal bar
+
+plot = s_data.plot.barh(x="Sell")
